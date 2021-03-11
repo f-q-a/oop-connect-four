515455aad53d96ce148a7d669cdaf61ee9c2602c
felipe <december1294@gmail.com>
2021-03-11 13:29:23 -0500

    "work on starting new game phase"

--- connect-four.js
+++ connect-four.js
@@ -1,12 +1,20 @@
-import { Game } from './game.js'
+import Game  from './game.js'
 
 let game = undefined;
+
+function updateUi(){
+    if(game === undefined){
+        document.getElementById(`board-holder`).classList.add(`is-invisible`);
+    }else{
+        document.getElementById(`board-holder`).classList.remove(`is-invisible`);
+    }
+}
 
 window.addEventListener('DOMContentLoaded', () => {
 
     const playerOne = document.getElementById('player-1-name');
     const playerTwo = document.getElementById('player-2-name');
-    
+
 
     document.getElementById('form-holder').addEventListener('keyup', () => {
 
@@ -17,9 +25,14 @@ window.addEventListener('DOMContentLoaded', () => {
         }
 
     })
+    document.getElementById(`new-game`).addEventListener(`click`, event => {
+        game = new Game('', '');
+        document.getElementById(`new-game`).disabled = true;
+        updateUi();
 
-    
+    })
 
-    
+
+
 
 })
--- game.js
+++ game.js
@@ -1,10 +1,16 @@
-class Game {
+
+export class Game {
     constructor (playerOne, playerTwo) {
         this.nameOne = playerOne;
         this.nameTwo = playerTwo;
+        this.currPlayer = playerOne;
     }
 
     getName () {
         return `${this.nameOne} vs ${this.nameTwo}`;
+    }
+
+    playInColumn() {
+
     }
 }
